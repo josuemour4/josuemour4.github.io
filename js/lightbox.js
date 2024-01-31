69c0ab800594222743501007ef5f543540a5d629
josuemour4 <154309452+josuemour4@users.noreply.github.com>
2024-01-31 22:06:17 +1100

    life is weird, but It is not the end yet

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,31 +1,67 @@
-
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+//Open Modal functions
+function openModalMTL() {
+  document.getElementById("mtlModal").style.display = "block";
 }
 
-function openModal2() {
-  document.getElementById("myModal2").style.display = "block";
+function jrOpenModal() {
+  document.getElementById("jrModal").style.display = "block";
 }
 
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-function closeModal2() {
-  document.getElementById("myModal2").style.display = "none";
+function neweraOpenModal() {
+  document.getElementById("neweraModal").style.display = "block";
 }
 
+function elecdataOpenModal() {
+  document.getElementById("elecdataModal").style.display = "block";
+}
+
+function htsOpenModal() {
+  document.getElementById("htsModal").style.display = "block";
+}
+
+function rwOpenModal() {
+  document.getElementById("rwModal").style.display = "block";
+}
+//End Open Modal functions
+
+//Close Modal functions
+function mtlCloseModal() {
+  document.getElementById("mtlModal").style.display = "none";
+}
+function jrCloseModal() {
+  document.getElementById("jrModal").style.display = "none";
+}
+function neweraCloseModal() {
+  document.getElementById("neweraModal").style.display = "none";
+}
+
+function elecdataCloseModal() {
+  document.getElementById("elecdataModal").style.display = "none";
+}
+
+function htsCloseModal() {
+  document.getElementById("htsModal").style.display = "none";
+}
+
+function rwCloseModal() {
+  document.getElementById("rwModal").style.display = "none";
+}
+//End Close Modal functions
+
+
+// MTL functionality
 var slideIndex = 1;
-showGallery(slideIndex);
+mtlGalerrySlide(slideIndex);
 
-function plusSlides(n) {
-  showGallery(slideIndex += n);
+function mtlPlusSlides(n) {
+  mtlGalerrySlide(slideIndex += n);
 }
 
-function galerrySlide(n) {
-  showGallery(slideIndex = n);
+function mtlGalerrySlide(n) {
+  mtlGalerrySlide(slideIndex = n);
 }
 
-function showGallery(n) {
+function mtlGalerrySlide(n) {
   var i;
   var slides = document.getElementsByClassName("gallery");
   var dots = document.getElementsByClassName("demo");
@@ -42,24 +78,26 @@ function showGallery(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+// End MTL functionality
 
 
-// Test
-showGalleryTest(slideIndex);
+// jr functionality
+var slideIndex = 1;
+jrGalerrySlide(slideIndex);
 
-function plusSlidesTest(n) {
-  showGalleryTest(slideIndex += n);
+function jrPlusSlides(n) {
+  jrGalerrySlide(slideIndex += n);
 }
 
-function galerrySlideTest(n) {
-  showGalleryTest(slideIndex = n);
+function jrGalerrySlide(n) {
+  jrGalerrySlide(slideIndex = n);
 }
 
-function showGalleryTest(n) {
+function jrGalerrySlide(n) {
   var i;
-  var slides = document.getElementsByClassName("gallery2");
-  var dots = document.getElementsByClassName("demo2");
-  var captionText = document.getElementById("caption2");
+  var slides = document.getElementsByClassName("jrElectrical");
+  var dots = document.getElementsByClassName("jrDemo");
+  var captionText = document.getElementById("jrCaption");
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
@@ -72,3 +110,35 @@ function showGalleryTest(n) {
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+// End jr functionality
+
+// new era functionality
+var slideIndex = 1;
+neweraGalerrySlide(slideIndex);
+
+function neweraPlusSlides(n) {
+  neweraGalerrySlide(slideIndex += n);
+}
+
+function neweraGalerrySlide(n) {
+  neweraGalerrySlide(slideIndex = n);
+}
+
+function neweraGalerrySlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("newera");
+  var dots = document.getElementsByClassName("neweraDemo");
+  var captionText = document.getElementById("neweraCaption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+// new era functionality
