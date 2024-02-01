9cfed0f2bbe8268f5bdcb7ccdf19b0f51370768f
josuemour4 <154309452+josuemour4@users.noreply.github.com>
2024-02-01 22:05:59 +1100

    :)

--- js/test.js
+++ js/test.js
@@ -1,33 +1,73 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-
-function showOption(n) {
-
-    if (n == 1) {
-        var img = document.getElementById("cobra");
-        var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-        img.onclick = function () {
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
-        }
-
+ function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function openModal2() {
+    document.getElementById("myModal2").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  function closeModal2() {
+    document.getElementById("myModal2").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showGallery(slideIndex);
+  
+  function plusSlides(n) {
+    showGallery(slideIndex += n);
+  }
+  
+  function galerrySlide(n) {
+    showGallery(slideIndex = n);
+  }
+  
+  function showGallery(n) {
+    var i;
+    var slides = document.getElementsByClassName("gallery");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-
-
-
-}
-
-function currentOptionl(n) {
-    showOption(n);
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
+  
+  
+  // Test
+  showGalleryTest(slideIndex);
+  
+  function plusSlidesTest(n) {
+    showGalleryTest(slideIndex += n);
+  }
+  
+  function galerrySlideTest(n) {
+    showGalleryTest(slideIndex = n);
+  }
+  
+  function showGalleryTest(n) {
+    var i;
+    var slides = document.getElementsByClassName("gallery2");
+    var dots = document.getElementsByClassName("demo2");
+    var captionText = document.getElementById("caption2");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
